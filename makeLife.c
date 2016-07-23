502c16785fc38bcfdd40078a2324360f04e73647
Neutrinoant <kyh941031@naver.com>
2016-07-23 16:27:59 +0900

    Update makeLife.c
    Encapsulation

--- makeLife.c
+++ makeLife.c
@@ -15,34 +15,34 @@ int c2my(int cony)
 
 int isInterior(MAP *map, int mx, int my)
 {
-	if ((1<=mx && mx<map->col-2) && (1<=my && my<map->row-2))
+	if ((1<=mx && mx<GetCol(map)-2) && (1<=my && my<GetRow(map)-2))
 		return 1;
 	return 0;
 }
 
 int isBoundary(MAP *map, int mx, int my)
 {
-	if ((mx==1 || mx==map->col-2) || (my==1 && my==map->row-2))
+	if ((mx==1 || mx==GetCol(map)-2) || (my==1 && my==GetRow(map)-2))
 		return 1;
 	return 0;
 }
 
 void modifyNearLife(MAP *map, int x, int y, int n)
 {
-	map->life[x-1][y-1].nearlife+=n;
-	map->life[x-1][y].nearlife+=n;
-	map->life[x-1][y+1].nearlife+=n;
-	map->life[x][y-1].nearlife+=n;
-	map->life[x][y+1].nearlife+=n;
-	map->life[x+1][y-1].nearlife+=n;
-	map->life[x+1][y].nearlife+=n;
-	map->life[x+1][y+1].nearlife+=n;
+	SetNear(GetLife(map,x-1,y-1), GetNear(GetLife(map,x-1,y-1)) + n);
+	SetNear(GetLife(map,x-1,y), GetNear(GetLife(map,x-1,y)) + n);
+	SetNear(GetLife(map,x-1,y+1), GetNear(GetLife(map,x-1,y+1)) + n);
+	SetNear(GetLife(map,x,y-1), GetNear(GetLife(map,x,y-1)) + n);
+	SetNear(GetLife(map,x,y+1), GetNear(GetLife(map,x,y+1)) + n);
+	SetNear(GetLife(map,x+1,y-1), GetNear(GetLife(map,x+1,y-1)) + n);
+	SetNear(GetLife(map,x+1,y), GetNear(GetLife(map,x+1,y)) + n);
+	SetNear(GetLife(map,x+1,y+1), GetNear(GetLife(map,x+1,y+1)) + n);
 }
 
 void makeLife(MAP *map)
 {
 	UNIC ch;
-	int x, y;  // 2d position on map
+	int x, y;  // position on map
 	int n;
 
 	gotoxy(3,2);
@@ -54,22 +54,22 @@ void makeLife(MAP *map)
 
 		if (uniequal(ch, UP) && y != 1)
 			gotoxy(curposx(), curposy()-1);
-		else if (uniequal(ch, DOWN) && y != map->row-2)
+		else if (uniequal(ch, DOWN) && y != GetRow(map)-2)
 			gotoxy(curposx(), curposy()+1);
 		else if (uniequal(ch, LEFT) && x != 1)
 			gotoxy(curposx()-2, curposy());
-		else if (uniequal(ch, RIGHT) && x != map->col-2)
+		else if (uniequal(ch, RIGHT) && x != GetCol(map)-2)
 			gotoxy(curposx()+2, curposy());
 		else if (uniequal(ch, SPACE))
 		{
-			if (map->life[x][y].state == DEATH) {
-				map->life[x][y].state = LIVE;
+			if (GetState(GetLife(map,x,y)) == DEATH) {
+				SetState(GetLife(map,x,y), LIVE);
 				printf("■");  // curposx+=2
 				gotoxy(curposx()-2, curposy());
 				n = 1;
 			}
 			else {
-				map->life[x][y].state = DEATH;
+				SetState(GetLife(map,x,y), DEATH);
 				printf("□");  // curposx+=2
 				gotoxy(curposx()-2, curposy());
 				n = -1;

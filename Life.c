e1b2e48cf600275a891867bd86f38d5e47cd46d8
Neutrinoant <kyh941031@naver.com>
2016-07-23 16:24:50 +0900

    Update Life.c
    Encapsulation

--- Life.c
+++ Life.c
@@ -1,3 +1,21 @@
 #include "Life.h"
 
-//
+int GetState(LIFE *life)
+{
+	return life->state;
+}
+
+int GetNear(LIFE *life)
+{
+	return life->nearlife;
+}
+
+void SetState(LIFE *life, int state)
+{
+	life->state = state;
+}
+
+void SetNear(LIFE *life, int near)
+{
+	life->nearlife = near;
+}

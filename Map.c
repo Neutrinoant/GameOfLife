49e6f08c2da85476531f49b60a109ac92b9a1ae5
Neutrinoant <kyh941031@naver.com>
2016-07-23 16:30:40 +0900

    Update Map.c
    Encapsulation

--- Map.c
+++ Map.c
@@ -6,19 +6,19 @@
 #include "Life.h"
 #include "Map.h"
 
-void initMap(MAP *map)
+void MapInit(MAP *map)
 {
 	map->life = NULL;
 	map->row = 0;
 	map->col = 0;
 }
 
-void makeMap(MAP *map, CONSOLE *cons)
+void MakeMap(MAP *map, CONSOLE *cons)
 {
 	int i, j;
 	
-	map->col = cons->cols/2;  // unicode /2
-	map->row = cons->lines-1; // console boundary -1
+	map->col = GetCols(cons)/2;  // unicode /2
+	map->row = GetLines(cons)-1; // console boundary -1
 
 	// column:i, row:j //
 	map->life = (LIFE **)malloc(map->col * sizeof(LIFE));
@@ -29,13 +29,38 @@ void makeMap(MAP *map, CONSOLE *cons)
 	{
 		for (j=0; j<map->row; j++)
 		{
-			map->life[i][j].state = DEATH;
-			map->life[i][j].nearlife = 0;
+			SetState(&(map->life[i][j]), DEATH);
+			SetNear(&(map->life[i][j]), 0);
 		}
 	}
 }
 
-void destroyMap(MAP *map)
+int GetRow(MAP *map)
+{
+	return map->row;
+}
+
+int GetCol(MAP *map)
+{
+	return map->col;
+}
+
+LIFE *GetLife(MAP *map, int x, int y)
+{
+	return &(map->life[x][y]);
+}
+
+void SetRow(MAP *map, int row)
+{
+	map->row = row;
+}
+
+void SetCol(MAP *map, int col)
+{
+	map->col = col;
+}
+
+void DestroyMap(MAP *map)
 {
 	int i;
 	for (i=0; i<map->col; i++)

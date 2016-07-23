a6461f55a3098a49d7267233d6a367a5d5af988a
Neutrinoant <kyh941031@naver.com>
2016-07-23 16:45:50 +0900

    Update Map.h
    Add function GetLastx(), GetLasty().
    + Save recent position of cursor

--- Map.h
+++ Map.h
@@ -8,15 +8,21 @@ typedef struct map {
 	LIFE **life;
 	int row;
 	int col;
+	int lastx;
+	int lasty;
 } MAP;
 
 void MapInit(MAP *map);
 void MakeMap(MAP *map, CONSOLE *cons);
 int GetRow(MAP *map);
 int GetCol(MAP *map);
+int GetLastx(MAP *map);
+int GetLasty(MAP *map);
 LIFE *GetLife(MAP *map, int x, int y);
 void SetRow(MAP *map, int row);
 void SetCol(MAP *map, int col);
+void SetLastx(MAP *map, int mx);
+void SetLasty(MAP *map, int my);
 void DestroyMap(MAP *map);
 
 #endif
